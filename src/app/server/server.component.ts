44a5ee92455f18532465ae45b5d0b430746061d5
Abraham Mejia <aimejia4@gmail.com>
2023-10-03 02:23:17 -0500

    Completed Binding to Custom Properties

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
+  @Input() element: {type: string, name:string, content:string};
+
+  constructor(){
+  }
 
 }
